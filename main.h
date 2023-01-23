fe42085539855f9e700aaa10d03284fdb6335def
amlaksil <silamlakdesye@gmail.com>
2023-01-23 21:35:15 +0300

    Add main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef _PRINTF_
+#define _PRINTF_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int (*check_format_specifier(const char *format))(va_list);
+
+/**
+  * struct function - containes character and function pointer
+  * @tmp: character pointer which points to valid format specifier
+  * @f: function pointer which points format specifier function 
+  */
+
+typedef struct function
+{
+	char *tmp;
+	int (*f)(va_list);
+}ftype;
+int print_char(va_list);
+int print_str(va_list);
+int print_percent(va_list);
+#endif
