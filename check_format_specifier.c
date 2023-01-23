feeb5084300552623115d26b15c009cfb2206ced
amlaksil <silamlakdesye@gmail.com>
2023-01-23 21:23:23 +0300

    Add a program that checks for valid format specifier

---
+++ check_format_specifier.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * check_format_specifier - checks the character if it is a valid format
+ * specifier and assigns an appropriate function for printing
+ * @format: format specifier
+ * Return: function pointer
+ */
+int (*check_format_specifier(const char *format))(va_list)
+{
+	int i = 0;
+	ftype form_array[] = {{"c", print_char},
+			      {"s", print_str},
+			      {"%", print_percent},
+			      {NULL, NULL}};
+
+	while (form_array[i].tmp != NULL)
+	{
+		if (*(form_array[i].tmp) == *format)
+		{
+			return (form_array[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
