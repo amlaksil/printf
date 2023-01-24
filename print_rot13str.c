3633438900ef374e42d9056141e06cbb898dd02c
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-01-24 23:46:23 +0000

    bug fix rot13

--- print_rot13str.c
+++ print_rot13str.c
@@ -1,45 +1,33 @@
 #include "main.h"
 /**
- * rot13 - it encodes the given string by using key = 13
- * @a: the address of string to be encoded
- * Return: the encoded string
- */
-char *rot13(char *a)
-{
-	int i;
-
-	for (i = 0; a[i] != '\0'; i++)
-	{
-	if (a[i] >= 'a' && a[i] <= 'z')
-	{
-	a[i] = (((a[i] - 'a') + 13) % 26) + 'a';
-	}
-	if (a[i] >= 'A' && a[i] <= 'Z')
-	{
-		a[i] = (((a[i] - 'A') + 13) % 26) + 'A';
-	}
-	}
-	return (a);
-}
-
-/**
- * print_rot13str - prints a string to int rot13 format
+ * print_rot13str - prints a string to rot13 format
  * @args: variadic parameter
  *
  * Return: number of characters printed
  */
 int print_rot13str(va_list args)
 {
-	int count = 0;
-	char *str;
+	int count = 0, i;
+	char *s;
+	
+	s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(nill)";
 
-	str = va_arg(args, char *);
-	if (str == NULL)
-		str = "(null)";
-	str = rot13(str);
-	while (str[count] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(str[count]);
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+	s[i] = (((s[i] - 'a') + 13) % 26) + 'a';
+	}
+	if (s[i] >= 'A' && s[i] <= 'Z')
+	{
+		s[i] = (((s[i] - 'A') + 13) % 26) + 'A';
+	}
+	}
+	while (s[count] != '\0')
+	{
+		_putchar(s[count]);
 		count++;
 	}
 	return (count);
