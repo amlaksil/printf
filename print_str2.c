3321992167e8951f73eddfaafa522b43d3e25579
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-01-24 17:19:03 +0000

    adding print_str2.c

---
+++ print_str2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_string2 - it prints a string includeing the non printable ones
+ * @s: the string to be printed
+ * Return: the number of characters printed
+ */
+int print_str2(va_list args)
+{
+        int i, count = 0;
+	char n[] = "0123456789ABCDEF";
+	char *s = va_arg(args,char *);
+
+        if (s == NULL)
+        {
+                return (0);
+        }
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                if ((s[i] > 0 && s[i] < 32) || s[i] >= 127)
+                {
+                        _putchar(92);
+                        _putchar('x');
+                        _putchar(n[s[i] / 16]);
+                        _putchar(n[s[i] % 16]);
+			count += 4;
+		}
+		else
+		{
+			_putchar(s[i]);
+			count++;
+		}
+	}
+	return (count);
+}
